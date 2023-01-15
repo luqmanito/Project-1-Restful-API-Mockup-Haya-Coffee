2d53771ccbcee347c403d0400f607c7ce87b7803
luqmanito <luqmangrahito@gmail.com>
2023-01-15 07:52:57 +0700

    fixfinal

--- src/helpers/mail.js
+++ src/helpers/mail.js
@@ -15,18 +15,18 @@ var CryptoJS = require("crypto-js");
 // });
 
 // const sendEmail = (emailReceiver, id) => {
-  // let _secretKey = SECRETKEY;
+// let _secretKey = SECRETKEY;
 //   console.log("kirim email ngga");
 //   console.log(emailReceiver);
 //   console.log(id);
 //   console.log(_secretKey);
 //   console.log("dari env" + SECRETKEY);
 
-  // const encryptID = CryptoJS.AES.encrypt(`${id}`, `${_secretKey}`).toString();
+// const encryptID = CryptoJS.AES.encrypt(`${id}`, `${_secretKey}`).toString();
 
-  // const slashNone = encryptID.replaceAll("/", "ito");
+// const slashNone = encryptID.replaceAll("/", "ito");
 //   console.log(slashNone);
-  // let body2 = `http://localhost:3000/activation/${slashNone}`;
+// let body2 = `http://localhost:3000/activation/${slashNone}`;
 //   console.log(body2);
 //   const options = {
 //     from: `"Haya-Coffee-Shops" <${EMAIL}>`,
@@ -43,47 +43,87 @@ var CryptoJS = require("crypto-js");
 const nodemailer = require("nodemailer");
 
 // async..await is not allowed in global scope, must use a wrapper
-async function main(emailReceiver, id) {
-  let _secretKey = SECRETKEY;
-  const encryptID = CryptoJS.AES.encrypt(`${id}`, `${_secretKey}`).toString();
-  const slashNone = encryptID.replaceAll("/", "ito");
-  let body2 = `http://localhost:3000/activation/${slashNone}`;
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  // let testAccount = await nodemailer.createTestAccount();
-console.log('dari cont',emailReceiver, id);
-  // create reusable transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-  service: "Zoho",
-  host: "smtp.zoho.com",
-  port: 465,
-  secure: true, //ssl
-  auth: {
-    user: EMAIL,
-    pass: PASSWORD,
+// async function main(emailReceiver, id) {
+//   let _secretKey = SECRETKEY;
+//   const encryptID = CryptoJS.AES.encrypt(`${id}`, `${_secretKey}`).toString();
+//   const slashNone = encryptID.replaceAll("/", "ito");
+//   let body2 = `http://localhost:3000/activation/${slashNone}`;
+//   // Generate test SMTP service account from ethereal.email
+//   // Only needed if you don't have a real mail account for testing
+//   // let testAccount = await nodemailer.createTestAccount();
+// console.log('dari cont',emailReceiver, id);
+//   // create reusable transporter object using the default SMTP transport
+// const transporter = nodemailer.createTransport({
+//   service: "Zoho",
+//   host: "smtp.zoho.com",
+//   port: 465,
+//   secure: true, //ssl
+//   auth: {
+//     user: EMAIL,
+//     pass: PASSWORD,
+//   },
+// });
+
+//   // send mail with defined transport object
+//   let info = await transporter.sendMail({
+//     from: `"Haya-Coffee-Shops" <${EMAIL}>`, // sender address
+//     to: emailReceiver, // list of receivers
+//     subject: "Email Activation:", // Subject line
+//     text: `This is link ${body2} to activate your account :`, // plain text body
+//     // html: "<b>Hello New User?</b>", // html body
+//   });
+
+//   console.log("Message sent: %s", info.messageId);
+//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+//   // Preview only available when sending through an Ethereal account
+//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+// }
+
+// // main().catch(console.error);
+
+// // sendEmail(`luqmangrahito@gmail.com`)
+
+// module.exports = main;
+
+module.exports = {
+  mailSender: (emailReceiver, id) => {
+    return new Promise((resolve, reject) => {
+      // const accessToken = OAuth2Client.getAccessToken;
+
+      const transporter = nodemailer.createTransport({
+        service: "Zoho",
+        host: "smtp.zoho.com",
+        port: 465,
+        secure: true, //ssl
+        auth: {
+          user: EMAIL,
+          pass: PASSWORD,
+        },
+      });
+      let _secretKey = SECRETKEY;
+      const encryptID = CryptoJS.AES.encrypt(`${id}`, `${_secretKey}`).toString()
+      const slashNone = encryptID.replaceAll("/", "ito");
+      let body2 = `http://localhost:3000/activation/${slashNone}`
+
+      const mailOption = {
+        from: `"Haya-Coffee-Shops" <${EMAIL}>`, // sender address
+        to: emailReceiver, // list of receivers
+        subject: "Email Activation:", // Subject line
+        text: `This is link ${body2} to activate your account :`, // plain text body
+      };
+
+      transporter.sendMail(mailOption, (err, info) => {
+        if (err) {
+          console.log(err);
+          return reject({ status: 500, msg: "Internal Server Error" });
+        }
+        return resolve({
+          status: 200,
+          msg: "Success, check email to verify",
+        });
+      });
+    });
   },
-});
-
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: `"Haya-Coffee-Shops" <${EMAIL}>`, // sender address
-    to: emailReceiver, // list of receivers
-    subject: "Email Activation:", // Subject line
-    text: `This is link ${body2} to activate your account :`, // plain text body
-    // html: "<b>Hello New User?</b>", // html body
-  });
-
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-// main().catch(console.error);
-
-// sendEmail(`luqmangrahito@gmail.com`)
-
-module.exports = main;
+};
